cb11998f4f6427e09635272e8ae2dc6289623268
icebear2n2 <icebear2n2@gmail.com>
2023-10-23 11:26:16 +0900

    feat: exception 을 통한 커스텀 예외 처리 구현

---
+++ src/main/java/com/icebear2n2/productservice/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.icebear2n2.productservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    DUPLICATED_CATEGORY_NAME(HttpStatus.CONFLICT, "CATEGORY NAME IS DUPLICATED."),
+    DUPLICATED_PRODUCT_NAME(HttpStatus.CONFLICT, "PRODUCT NAME IS DUPLICATED."),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY NOT FOUND."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT NOT FOUND."),
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
