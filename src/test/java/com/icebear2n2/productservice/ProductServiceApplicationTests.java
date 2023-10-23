9e8b22054ad2ce98d050d9f151d8f2da3381b052
icebear2n2 <icebear2n2@gmail.com>
2023-10-23 10:00:17 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/icebear2n2/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
