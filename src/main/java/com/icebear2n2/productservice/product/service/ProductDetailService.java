8ee192f683353c29d619bb0ed4ffaca6f673d60e
icebear2n2 <icebear2n2@gmail.com>
2023-10-23 13:47:53 +0900

    !HOTFIX: 서비스 로직 변경

--- src/main/java/com/icebear2n2/productservice/product/service/ProductDetailService.java
+++ src/main/java/com/icebear2n2/productservice/product/service/ProductDetailService.java
@@ -16,23 +16,22 @@ public class ProductDetailService {
     private final ProductDetailRepository productDetailRepository;
     private final ProductRepository productRepository;
 
-//    TODO: CREATE PRODUCT DETAIL
-
     public void createProductDetail(CreateProductDetailRequest createProductDetailRequest) {
+        Product product = productRepository.findByProductName(createProductDetailRequest.getProductName());
+
+        if (product == null) {
+            throw new ProductServiceException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+
+
+        if (productDetailRepository.existsByProduct(product)) {
+            throw new ProductServiceException(ErrorCode.DUPLICATED_PRODUCT_DETAIL);
+        }
+
         try {
-
-            ProductDetail productDetail = productDetailRepository.save(createProductDetailRequest.toEntity());
-
-            Product product = productRepository.findByProductName(createProductDetailRequest.getProductName());
-
-            if(product == null) {
-                throw new ProductServiceException(ErrorCode.PRODUCT_NOT_FOUND);
-            }
-
+            ProductDetail productDetail = createProductDetailRequest.toEntity();
             productDetail.setProduct(product);
-
             productDetailRepository.save(productDetail);
-
         } catch (Exception e) {
             throw new ProductServiceException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
