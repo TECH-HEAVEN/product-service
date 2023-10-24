b938074c7d5a3f9402899c208076d96908fb938f
icebear2n2 <icebear2n2@gmail.com>
2023-10-24 16:06:15 +0900

    feat: swagger config 파일 생성

---
+++ src/main/java/com/icebear2n2/productservice/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.icebear2n2.productservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition(
+        info = @Info(title = "Product-Service API 명세서",
+                description = "상품 카탈로그 서비스 API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+//    @Bean
+//    public OpenAPI openAPI(){
+//        SecurityScheme securityScheme = new SecurityScheme()
+//                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+//                .in(SecurityScheme.In.HEADER).name("Authorization");
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+//
+//        return new OpenAPI()
+//                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+//                .security(Arrays.asList(securityRequirement));
+//    }
+
+}
